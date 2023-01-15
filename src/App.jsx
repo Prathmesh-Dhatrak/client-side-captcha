0e109fb26d838d35ae30feba0f3ba792a13e2640
Prathmesh Santosh Dhatrak <prathmesh101dhatrak@gmail.com>
2023-01-15 09:45:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Box, Flex } from "@chakra-ui/react";
+function App() {
+  return (
+    <div className="App">
+      <Flex align="center" justify="center">
+        Flex Container
+      </Flex>
+    </div>
+  );
+}
+
+export default App;
+
